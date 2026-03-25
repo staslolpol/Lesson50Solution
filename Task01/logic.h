60966897e5a6e2dd7d3928f890a39416fc1e0263
staslolpol <staspriho9@gmail.com>
2026-03-25 18:10:27 +0300

    1

--- Task01/logic.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-
-#define DEFAUT_SIZE 100'000
-
-using namespace std;
---
+++ Task01/test01.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//using namespace std;
+//
+//void test(int counter) {
+//	cout << counter << endl;
+//	test(++counter);
+//}
+//
+//int main() {
+//	int counter = 1;
+//	test(counter);
+//
+//	return 0;
+//}
