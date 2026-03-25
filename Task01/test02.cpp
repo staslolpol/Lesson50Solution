2d9ab46ea1843332671b60698a1efdcfe9a59c8f
staslolpol <staspriho9@gmail.com>
2026-03-25 18:30:13 +0300

    2

--- Task01/test02.cpp
+++ Task01/test02.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-using namespace std;
-
-long long factorial(int number) {
-	if (number < 0) {
-		return -1;
-	} 
-
-	if (number <= 1) {
-		return 1;
-	}
-
-	return number * factorial(number - 1);
-}
-
-void test(int counter) {
-	cout << counter << endl;
-	test(++counter);
-}
-
-int main() {
-	cout << factorial(5);
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//long long factorial(int number) {
+//	if (number < 0) {
+//		return -1;
+//	} 
+//
+//	if (number <= 1) {
+//		return 1;
+//	}
+//
+//	return number * factorial(number - 1);
+//}
+//
+//void test(int counter) {
+//	cout << counter << endl;
+//	test(++counter);
+//}
+//
+//int main() {
+//	cout << factorial(5);
+//	return 0;
+//}
